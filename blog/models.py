e1cddea5cb01b70c6c12f51cc7edd9ff9d2ac7ea
williamchavula <rumbani@gmail.com>
2022-11-01 14:21:37 -0500

    add Post model and perform initial db migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils import timezone
+
+User = get_user_model()
+
+
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = "DF, Draft"
+        PUBLISHED = "PB, Published"
+
+    post_id = models.UUIDField(
+        default=uuid4, db_index=True, primary_key=True, editable=False, verbose_name="Blog Post ID",
+        help_text="ID of your blog post, automatically generated")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts")
+    title = models.CharField(max_length=100, verbose_name="Blog Post Title",
+                             help_text="This is the title of your blog post")
+    slug = models.SlugField(
+        max_length=150, help_text="This is the slug form of the title of your blog post", unique_for_date="created")
+    body = models.TextField(
+        help_text="This is the content of your blog post", verbose_name="Blog Post Content")
+    status = models.CharField(
+        max_length=20, choices=Status.choices, default=Status.DRAFT)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name="Blog Post Create Date")
+    updated = models.DateTimeField(
+        auto_now=True, verbose_name="Blog Post Uodate Date")
+
+    class Meta:
+        ordering = ["-publish"]
+        indexes = [models.Index(fields=["-publish"])]
+
+    def __str__(self) -> str:
+        return f"{self.post_id}, {self.title}"
